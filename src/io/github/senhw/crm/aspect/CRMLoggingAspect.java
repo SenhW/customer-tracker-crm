94c99cfde1a4e941eac7ebfbd551d82cc7c7ee54
SenhW <senhwang3@gmail.com>
2019-05-04 13:04:19 -0400

    AOP Setup AspectJ JAR and create Before advice to log method calls

---
+++ src/io/github/senhw/crm/aspect/CRMLoggingAspect.java
@@ -0,0 +1,45 @@
+package io.github.senhw.crm.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+	// setup logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	// setup pointcut declarations
+	@Pointcut("execution(* io.github.senhw.crm.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* io.github.senhw.crm.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* io.github.senhw.crm.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
+	
+	// add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// display method we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====>> in @Before: calling method: " + theMethod);
+		
+		// display the arguments to the method
+		
+	}
+	
+	// add @AfterReturning advice
+	
+}
